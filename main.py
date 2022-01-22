543bd6aba5786172f0e2c130a64bcfcf2c743c1f
Robert Coffie <rcoffie22@yahoo.com>
2022-01-22 22:25:38 +0000

    using get request

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def root():
-  return {'message':'hello world'}
+def root():
+  return {'message':'hello world'} 
+
+@app.get('/posts')
+def get_post():
+  return {'data':'this is your posts'}
