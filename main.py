27202c29f2cffce1bd4877b1cfa292b49af46678
Robert Coffie <rcoffie22@yahoo.com>
2022-01-23 03:29:51 +0000

    getting data with pydantic

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel 
+
 app = FastAPI()
+
+
+class Post(BaseModel):
+  title: str 
+  content: str 
 
 @app.get('/')
 def root():
@@ -8,3 +15,9 @@ def root():
 @app.get('/posts')
 def get_post():
   return {'data':'this is your posts'}
+
+
+@app.post('/createposts')
+def create_posts(new_post:Post):
+  print(new_post)
+  return {'data':'post added'}
